dcf6dd4041870d448dc0607d1cdd4271d699a57c
FBK <84973711+115AKIRA@users.noreply.github.com>
2022-01-19 15:00:20 +0100

    Update quiz4.php

--- quiz4.php
+++ quiz4.php
@@ -10,32 +10,7 @@ if (isset($_POST['btn_valider'])) {
         echo 'Veuillez selectionner un bouton !';
     } else {
         $boutonval = $_POST['radio'];
-
-        $v_0 = 0;
-        $v_1 = 0;
-        $v_2 = 0;
-        $v_3 = 0;
-        $v_4 = 0;
-
-        switch($boutonval) {
-            case 0:
-                $v_0 = 1;
-                break;
-            case 1:
-                $v_1 = 1; 
-                break;
-            case 2:
-                $v_2 = 1; 
-                break;
-            case 3:
-                $v_3 = 1;
-                break;
-            case 4:
-                $v_4 = 1;
-                break;
-        }
-
-        echo 'Consommation alimentaire : '.((($v_0*213.613)+($v_1*600.251)+($v_2*1028.4)+($v_3*1632)+($v_4*1888.335))/12).' kgCO2eq';
+        echo 'Consommation alimentaire : '.($boutonval/12).' kgCO2eq';
     }
 }
 ?>
@@ -67,31 +42,31 @@ if (isset($_POST['btn_valider'])) {
                 <input checked type="radio"
                        id="0"
                        name="radio"
-                       value="0">Jamais</input>
+                       value="213.613">Jamais</input>
             </label>
             <label for="1">
                 <input type="radio"
                        id="1"
                        name="radio"
-                       value="1">Moins d'1 fois par semaine</input>
+                       value="600.251">Moins d'1 fois par semaine</input>
             </label>
             <label for="2">
                 <input type="radio"
                        id="2"
                        name="radio"
-                       value="2">1 fois par semaine</input>
+                       value="1028.4">1 fois par semaine</input>
             </label>
             <label for="3">
                 <input type="radio"
                        id="3"
                        name="radio"
-                       value="3">2 fois par semaine</input>
+                       value="1632">2 fois par semaine</input>
             </label>
             <label for="4">
                 <input type="radio"
                        id="4"
                        name="radio"
-                       value="4">Plus de 2 fois par semaine</input>
+                       value="1888.335">Plus de 2 fois par semaine</input>
             </label>
         </div>
 
