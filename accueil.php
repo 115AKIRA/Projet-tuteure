f86e98127426e2d8fd96a4d103bf16d2d0279766
FBK <84973711+115AKIRA@users.noreply.github.com>
2022-01-20 18:24:48 +0100

    accueil

---
+++ accueil.php
@@ -0,0 +1,37 @@
+<html>
+<html lang="fr">
+  
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="accueil.css"/>
+    <title>Quiz</title>
+ 
+</head>
+  
+<body>
+    <header></header>
+
+    <section>
+        <figure id="nuage1"> <a href="missions.php"> <img class = "nuage" src="img/nuag.png"> </a> <figcaption> Missions </figcaption> </figure>
+        <figure id="nuage2"> <a href="profil.php"> <img class = "nuage" src="img/nuag.png"> </a>  <figcaption> Profil </figcaption> </figure>
+        <figure id="nuage3"> <a href="articles.php"> <img class = "nuage" src="img/nuag.png"> </a>  <figcaption> Articles</figcaption> </figure>
+        <figure id="nuage4"> <a href="jeux.php"> <img class = "nuage" src="img/nuag.png"> </a>  <figcaption> Jeux </figcaption> </figure>
+    </section>
+
+<footer>
+    <nav>
+        <ul>
+            <li><a href="accueil.php"> <img class="menu" src="img/menu.png"> </a></li>
+            <li><a href="missions.php"> <img class ="menu" src="img/missions.png"> </a></li>
+            <li><a href="profil.php"> <img class = "menu" src="img/profil.png"> </a></li>
+            <li><a href="articles.php"> <img class = "menu" src="img/articles.png"> </a></li>
+            <li><a href="jeux.php"> <img class = "menu" src="img/jeux.png"> </a></li>
+        </ul> 
+    </nav>
+</footer>
+
+</body>
+
+</html>
